78ad5951a688e4cb1844d951dd360e1fd21c4df9
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2023-02-22 18:30:04 +0100

    feat(service): add catch exception

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import {
@@ -19,44 +19,52 @@ export class PokemonService {
   constructor(private readonly httpService: HttpService) {}
 
   async getAllPokemons() {
-    const { data } = await firstValueFrom(
-      this.httpService.get<PokemonsApiResponse>(
-        `${API_URL}?limit=100000&offset=0`,
-      ),
-    );
-    const pokemons = data.results.map((pokemon, index) => ({
-      name: pokemon.name,
-      order: index + 1,
-      imgSrc: `${SPRITE_URL}${index + 1}${SPRITE_EXTENSION}`,
-    }));
-    return { pokemons, count: data.count };
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get<PokemonsApiResponse>(
+          `${API_URL}?limit=100000&offset=0`,
+        ),
+      );
+      const pokemons = data.results.map((pokemon, index) => ({
+        name: pokemon.name,
+        order: index + 1,
+        imgSrc: `${SPRITE_URL}${index + 1}${SPRITE_EXTENSION}`,
+      }));
+      return { pokemons, count: data.count };
+    } catch (e) {
+      throw new NotFoundException();
+    }
   }
 
   async getPokemon(name: string) {
-    const { data } = await firstValueFrom(
-      this.httpService.get<PokemonApiReponse>(`${API_URL}/${name}`),
-    );
-    const types = data.types.map(({ type }) => type.name);
-    const moves = data.moves.slice(0, MAX_MOVES).map(({ move }) => move.name);
-    const stats = data.stats.map((stat) => ({
-      name: stat.stat.name,
-      base: stat.base_stat,
-    }));
-    const height = data.height / CONVERT_HEIGHT_INDICES;
-    const weight = data.weight / CONVERT_WEIGHT_INDICES;
-    const id = data.id;
-    const imgSrc = `${SPRITE_URL}${data.id}${SPRITE_EXTENSION}`;
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get<PokemonApiReponse>(`${API_URL}/${name}`),
+      );
+      const types = data.types.map(({ type }) => type.name);
+      const moves = data.moves.slice(0, MAX_MOVES).map(({ move }) => move.name);
+      const stats = data.stats.map((stat) => ({
+        name: stat.stat.name,
+        base: stat.base_stat,
+      }));
+      const height = data.height / CONVERT_HEIGHT_INDICES;
+      const weight = data.weight / CONVERT_WEIGHT_INDICES;
+      const id = data.id;
+      const imgSrc = `${SPRITE_URL}${data.id}${SPRITE_EXTENSION}`;
 
-    return {
-      pokemon: {
-        imgSrc,
-        types,
-        moves,
-        stats,
-        height,
-        weight,
-        id,
-      },
-    };
+      return {
+        pokemon: {
+          imgSrc,
+          types,
+          moves,
+          stats,
+          height,
+          weight,
+          id,
+        },
+      };
+    } catch (e) {
+      throw new NotFoundException();
+    }
   }
 }
