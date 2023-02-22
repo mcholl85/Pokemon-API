76ff63d405381e293b05c9d199521bd235dfcf86
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2023-02-22 15:12:32 +0100

    feat(module): add pokemon module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonController } from './pokemon/pokemon.controller';
+import { PokemonService } from './pokemon/pokemon.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PokemonController],
+  providers: [AppService, PokemonService],
 })
 export class AppModule {}
